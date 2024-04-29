fec3d698f1149d31e880c9ed640b52aae0141dc1
ulitsaRaskolnikova <malyshev.2005n@gmail.com>
2024-04-29 14:48:58 +0300

    Just commit

--- src/fileSystem/XMLHandler.java
+++ src/fileSystem/XMLHandler.java
@@ -12,13 +12,11 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
 
 /**
  * XMLHandler handles xml file and get it's input.
